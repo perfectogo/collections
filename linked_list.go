11050f1c9101db8871880b8b792c0607c6706add
jasurbek Shamsitdinov <jasurbek.go.dev@gmail.com>
2024-07-17 21:14:04 +0500

    print

--- linked_list.go
+++ linked_list.go
@@ -1,5 +1,7 @@
 package collections
 
+import "errors"
+
 // Node represents a single node in the linked list
 type Node[T any] struct {
 	Value T
@@ -22,10 +24,35 @@ func NewLinkedList[T any]() *LinkedList[T] {
 	return &LinkedList[T]{}
 }
 
+// func (l *LinkedList[T]) Append(value T) {
+
+// 	var newNode = NewNode(value)
+
+// 	for node := l.HeadNode; node != nil; node = node.Next {
+
+// 	}
+// }
+
+func (l *LinkedList[T]) PreAppend() {
+
+}
+
 // GetLength returns the length of the linked list
 func (l *LinkedList[T]) GetLength() int {
 	if l != nil {
 		return l.length
 	}
 	return 0
+}
+
+func (ll *LinkedList[T]) Print() error {
+	if ll == nil {
+		return errors.New("the linked list is nil")
+	}
+
+	for node := ll.HeadNode; node != nil; node = node.Next {
+		println(node.Value)
+	}
+
+	return nil
 }
