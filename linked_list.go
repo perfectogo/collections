8efd4e01f595f78ac3f694ae8ec0413e4ea88b3b
jasurbek Shamsitdinov <jasurbek.go.dev@gmail.com>
2024-07-18 11:07:30 +0500

    added methods

--- linked_list.go
+++ linked_list.go
@@ -1,6 +1,10 @@
 package collections
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 // Node represents a single node in the linked list
 type Node[T any] struct {
@@ -24,19 +28,6 @@ func NewLinkedList[T any]() *LinkedList[T] {
 	return &LinkedList[T]{}
 }
 
-// func (l *LinkedList[T]) Append(value T) {
-
-// 	var newNode = NewNode(value)
-
-// 	for node := l.HeadNode; node != nil; node = node.Next {
-
-// 	}
-// }
-
-func (l *LinkedList[T]) PreAppend() {
-
-}
-
 // GetLength returns the length of the linked list
 func (l *LinkedList[T]) GetLength() int {
 	if l != nil {
@@ -45,14 +36,91 @@ func (l *LinkedList[T]) GetLength() int {
 	return 0
 }
 
+// ...
 func (ll *LinkedList[T]) Print() error {
+
 	if ll == nil {
 		return errors.New("the linked list is nil")
 	}
 
+	var builder strings.Builder
+	builder.WriteString("[")
+
 	for node := ll.HeadNode; node != nil; node = node.Next {
-		println(node.Value)
+		builder.WriteString(fmt.Sprintf("%v", node.Value))
+		if node.Next != nil {
+			builder.WriteString(" ")
+		}
 	}
+
+	builder.WriteString("]")
+
+	fmt.Println(builder.String())
+	return nil
+}
+
+// this method for ....
+func (ll *LinkedList[T]) Append(value T) error {
+	var newNode = &Node[T]{
+		Value: value,
+		Next:  nil,
+	}
+
+	if ll.HeadNode == nil {
+		ll.HeadNode = newNode
+	} else {
+		currentNode := ll.HeadNode
+		for currentNode.Next != nil {
+			currentNode = currentNode.Next
+		}
+		currentNode.Next = newNode
+	}
+
+	ll.length++
+	return nil
+}
+
+// this method for ....
+func (ll *LinkedList[T]) PreAppend(value T) error {
+	var newNode = &Node[T]{
+		Value: value,
+	}
+
+	if ll.HeadNode == nil {
+		ll.HeadNode = newNode
+	} else {
+		newNode.Next = ll.HeadNode
+		ll.HeadNode = newNode
+	}
+
+	ll.length++
+	return nil
+}
+
+func (ll *LinkedList[T]) Insert(position int, value T) error {
+
+	if position < 1 || position > ll.length+1 {
+		return fmt.Errorf("insert: Index out of bounds")
+	}
+
+	newNode := &Node[T]{Value: value}
+	var prevNode, currentNode = &Node[T]{}, ll.HeadNode
+
+	for position > 1 {
+		prevNode = currentNode
+		currentNode = currentNode.Next
+		position = position - 1
+	}
+
+	if prevNode != nil {
+		prevNode.Next = newNode
+		newNode.Next = currentNode
+	} else {
+		newNode.Next = currentNode
+		ll.HeadNode = newNode
+	}
+
+	ll.length++
 
 	return nil
 }
